d48ba1427dd72808518cc6da1618c6d86115204b
JosephGonzalez03 <josephdg3@gmail.com>
2023-02-20 20:33:00 -0600

    refactor: Update demo file

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -12,12 +12,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_cartesian_2d(-10f32..10f32, -10f32..10f32)?;
 
     chart.configure_mesh().draw()?;
-    let b = GallifreyanLetter::NG(Polar::new(6.0, Degree(0.0)));
+    let b = GallifreyanLetter::CH(Polar::new(6.0, Degree(0.0)));
+    let c = GallifreyanLetter::CH(Polar::new(6.0, Degree(180.0)));
     chart.draw_series(LineSeries::new(
         b.letter().base().to_drawing().to_points().to_vec(),
         GREEN,
     ))?;
+    chart.draw_series(LineSeries::new(
+        c.letter().base().to_drawing().to_points().to_vec(),
+        GREEN,
+    ))?;
+    chart.draw_series(LineSeries::new(
+        b.letter().base().buffer(c.letter().base()).unwrap().to_points().to_vec(),
+        RED,
+    ))?;
     b.letter()
+        .modifiers()
+        .as_ref()
+        .expect("")
+        .to_drawings()
+        .iter()
+        .for_each(|drawing| {
+            //chart.draw_series(LineSeries::new(part.to_vec(), GREEN));
+            chart
+                .draw_series(
+                    LineSeries::new(drawing.to_points().to_vec(), RED.filled()).point_size(2),
+                )
+                .unwrap();
+        });
+    c.letter()
         .modifiers()
         .as_ref()
         .expect("")
